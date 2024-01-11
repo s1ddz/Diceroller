9b0e25e5349af47a9f8393cce75c1cd395db899d
Siddharrth Chaudhary <siddharthc67@outlook.com>
2024-01-11 22:53:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const roll=()=>{
 var a=Math.floor((Math.random()*6)+1);
-var url1="/images/dice"+a+".png";
+var url1="./images/dice"+a+".png";
 
 
 var b=Math.floor((Math.random()*6)+1);
-var url2="/images/dice"+b+".png";
+var url2="./images/dice"+b+".png";
 
 document.querySelectorAll("img")[0].setAttribute("src", url1);
 document.querySelectorAll("img")[1].setAttribute("src", url2);
